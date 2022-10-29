eda9e0156491215e8c8bd623d3b7bcf57630a683
Tabsheer <creativemk12@gmail.com>
2022-10-29 16:21:27 +0530

    react-weather-app created

---
+++ src/api.js
@@ -0,0 +1,13 @@
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6064666af9mshf4890795048bab0p1a9b2djsn12ae683f5c9d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1edacf17b7be168825c90f67d57cac04";
+
